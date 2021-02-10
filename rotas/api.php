5d296c0d0abe1cd0155a14ea195d65281b60013d
Guilherme Santana Coelho <guilhermecc0@hotmail.com>
2021-02-10 00:01:43 -0400

    Rotas das urls

--- rotas/api.php
+++ rotas/api.php
@@ -7,10 +7,29 @@
  */
 
 \core\Route::get('/escolas','EscolaControlador@getEscolas');
+\core\Route::get('/escolas/','EscolaControlador@getEscolas');
 \core\Route::post('/escola/adicionar','EscolaControlador@adicionarNovaEscola');
+\core\Route::post('/escola/adicionar/','EscolaControlador@adicionarNovaEscola');
 \core\Route::post('/escola/atualizar','EscolaControlador@atualizarEscola');
+\core\Route::post('/escola/atualizar/','EscolaControlador@atualizarEscola');
 \core\Route::post('/escola/excluir','EscolaControlador@excluirEscola');
+\core\Route::post('/escola/excluir/','EscolaControlador@excluirEscola');
 
-\core\Route::get('/turmas','TurmasControlador@getTurmas');
-\core\Route::get('/alunos','AlunosControlador@getAlunos');
-\core\Route::get('/alunos','AlunosControlador@getAlunos');
+\core\Route::get('/turmas','TurmaControlador@getTurmas');
+\core\Route::get('/turmas/','TurmaControlador@getTurmas');
+\core\Route::post('/escola/adicionar','TurmaControlador@adicionarNovaTurma');
+\core\Route::post('/escola/adicionar/','TurmaControlador@adicionarNovaTurma');
+\core\Route::post('/escola/atualizar','TurmaControlador@atualizarTurma');
+\core\Route::post('/escola/atualizar/','TurmaControlador@atualizarTurma');
+\core\Route::post('/escola/excluir','TurmaControlador@excluirTurma');
+\core\Route::post('/escola/excluir/','TurmaControlador@excluirTurma');
+
+\core\Route::get('/alunos','AlunoControlador@getAlunos');
+\core\Route::get('/alunos/','AlunoControlador@getAlunos');
+\core\Route::post('/aluno/adicionar','AlunoControlador@adicionarNovoAluno');
+\core\Route::post('/aluno/adicionar/','AlunoControlador@adicionarNovoAluno');
+\core\Route::post('/aluno/atualizar','AlunoControlador@atualizarAluno');
+\core\Route::post('/aluno/atualizar/','AlunoControlador@atualizarAluno');
+\core\Route::post('/aluno/excluir','AlunoControlador@excluirAluno');
+\core\Route::post('/aluno/excluir/','AlunoControlador@excluirAluno');
+
