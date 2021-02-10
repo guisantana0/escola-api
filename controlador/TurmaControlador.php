759d908c9a6af33c33cb890fdd2ebcd4f6cca7a8
Guilherme Santana Coelho <guilhermecc0@hotmail.com>
2021-02-10 00:01:25 -0400

    Controlador e Repositório Turma

---
+++ controlador/TurmaControlador.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guilherme
+ * Date: 07/02/2021
+ * Time: 16:22
+ */
+
+require_once REPOSITORIO_CAMINHO.'TurmaRepositorio.php';
+
+class TurmaControlador
+{
+
+    public function __construct()
+    {
+        $this->repositorio = new TurmaRepositorio();
+    }
+
+    public function getTurmas(){
+      echo $this->repositorio->obterTodasTurmas();
+    }
+
+    public function adicionarNovaTurma($requisicao){
+        echo $this->repositorio->adicionarNovaTurma($requisicao->post());
+    }
+
+    public function atualizarTurma($requisicao){
+        echo $this->repositorio->atualizarUmaTurma($requisicao->post());
+    }
+
+    public function excluirTurma($requisicao){
+        echo $this->repositorio->excluirTurma($requisicao->post());
+    }
+}
---
+++ repositorio/TurmaRepositorio.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guilherme
+ * Date: 09/02/2021
+ * Time: 23:54
+ **/
+
+require_once CLASSE_CORE_CAMINHO.'Repositorio.php';
+require_once CLASSE_CORE_CAMINHO.'ConstrutorQueryModelo.php';
+require_once MODELO_CAMINHO.'Turma.php';
+
+class TurmaRepositorio extends  \core\Repositorio
+{
+    public function __construct()
+    {
+        $umModelo = new Turma();
+        parent::__construct($umModelo);
+    }
+
+    public function obterTodasTurmas(){
+
+        $resultado = $this->consultar([]);
+        return json_encode($resultado);
+    }
+
+    public function adicionarNovaTurma($dados){
+        $this->adicionar($dados);
+    }
+
+    public function atualizarUmaTurma($dados){
+        $this->atualizar($dados);
+    }
+
+    public function excluirTurma($dados){
+        $this->excluirLogicamente($dados);
+    }
+}
